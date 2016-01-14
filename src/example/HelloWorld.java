6ecd509a228ae9e9be67645b9b0db5baa678a812
Ben Rusche <brusche@dgmail.com>
2016-01-13 21:35:39 -0500

    completed java 101

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -1,5 +1,44 @@
 package example;
 
 public class HelloWorld {
+	static String secondName = "jordan";
+
+	public static void main(String[] args) {
+		String builder = buildGreeting();
+		String otherMessage = "goodbye "+secondName;
+
+		if(!builder.equals("Hello ben")) {
+			displayMessage(builder);
+		}
+		else {
+			displayMessage(otherMessage);
+		}
+		int i = 0;
+
+		while( i <= 10) {
+			System.out.println("looping... at iteration "+ i);
+			i++;
+		}
+
+		for(int x =0; x<=10;x++){
+			System.out.println("for iteration = "+x);
+		}
+
+
+	}
+
+	private static String buildGreeting() {
+		String name = "ben";
+		String greeting = "Hello ";
+		String ret = greeting + name;
+
+		return ret;
+	}
+
+	private static void displayMessage(String message){
+		System.out.println(message);
+
+	}
+
 
 }
